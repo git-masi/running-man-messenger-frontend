5e846ab510f7324430671312e82b54825429bffe
Eric Masi <eric@gitmasi.com>
2020-04-11 09:50:31 -0400

    Add heading to test routes

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function Chat() {
   return (
-    <div>
-
-    </div>
+    <h1>Chat</h1>
   )
 }
--- src/components/Join.js
+++ src/components/Join.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function Join() {
   return (
-    <div>
-
-    </div>
+    <h1>Join</h1>
   )
 }
