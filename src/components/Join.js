3234b356587e52e0a8ad7b9305e1b045158add27
Eric Masi <eric@gitmasi.com>
2020-05-02 09:35:16 -0400

    Redirect user to a room on form submit

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,7 +1,48 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import useInputState from "../hooks/useInputState";
 
 export default function Join() {
+  const [nickname, setNickname] = useInputState("");
+  const [room, setRoom] = useInputState("");
+
+  const handleLinkClick = (e) =>
+    !nickname || !room ? e.preventDefault() : null;
+
   return (
-    <h1>Join</h1>
-  )
+    <main>
+      <h1>Join</h1>
+
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label>
+          Nickname
+          <input
+            required
+            type="text"
+            placeholder="Enter your nickname"
+            value={nickname}
+            onChange={setNickname}
+          />
+        </label>
+        <label>
+          Room
+          <input
+            required
+            type="text"
+            placeholder="Enter the room you'd like to join"
+            value={room}
+            onChange={setRoom}
+          />
+        </label>
+
+        <Link
+          onClick={handleLinkClick}
+          to={`/chat?nickname=${nickname}&room=${room}`}
+        >
+          <button type="submit">Join Chat</button>
+        </Link>
+      </form>
+    </main>
+  );
 }
--- src/hooks/useInputState.js
+++ src/hooks/useInputState.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
-export default function useInputState(initialValue) {
+export default (initialValue) => {
   const [state, setState] = useState(initialValue);
   const handleChange = (e) => setState(e.target.value);
   const reset = (resetValue = "") => setState(resetValue);
   return [state, handleChange, reset];
-}
+};
