9b3e0cbfefb8ba7bbff080a04581baf38dd041c0
Eric Masi <eric@gitmasi.com>
2020-05-02 16:17:13 -0400

    Send and display messages

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,17 @@
-import React, { useEffect } from "react";
+// Modules
+import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 import qs from "qs";
+
+// Utils
+import useInputState from "../hooks/useInputState";
 
 let socket;
 
 export default function Chat({ location }) {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useInputState("");
+
   const endPoint = "http://127.0.0.1:3333";
 
   useEffect(() => {
@@ -36,5 +43,43 @@ export default function Chat({ location }) {
     });
   }, []);
 
-  return <h1>Chat</h1>;
+  useEffect(() => {
+    socket.on("message", (msg) => {
+      setMessages([...messages, msg]);
+    });
+  }, [messages]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("newMessage", message, (err) => {
+      if (err) console.log(err);
+    });
+  };
+
+  const displayMessages = messages.map((m) => (
+    <li key={m.createdAt}>{m.text}</li>
+  ));
+
+  return (
+    <section>
+      <h1>Chat</h1>
+
+      <ul>{displayMessages}</ul>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Message
+          <input
+            required
+            type="text"
+            placeholder="Type your message"
+            value={message}
+            onChange={setMessage}
+          />
+        </label>
+
+        <button type="submit">Send</button>
+      </form>
+    </section>
+  );
 }
