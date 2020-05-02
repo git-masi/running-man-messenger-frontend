9d20446176f7bd8b8c61c77fb9f90497c8134a4a
Eric Masi <eric@gitmasi.com>
2020-05-02 10:30:27 -0400

    Create socket and join room on component load

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import io from "socket.io-client";
+import qs from "qs";
 
-export default function Chat() {
-  return (
-    <h1>Chat</h1>
-  )
+export default function Chat({ location }) {
+  const endPoint = "http://127.0.0.1:3333";
+
+  useEffect(() => {
+    const { nickname, room } = qs.parse(location.search, {
+      ignoreQueryPrefix: true,
+    });
+
+    let socket = io(endPoint);
+
+    socket.emit("join", { nickname, room }, (err) => {
+      console.log(err);
+    });
+
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
+  }, [location.search]);
+
+  return <h1>Chat</h1>;
 }
