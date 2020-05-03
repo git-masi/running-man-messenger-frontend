eec5d51c33da2b2082e8ce35f9791749c24e1928
Eric Masi <eric@gitmasi.com>
2020-05-03 11:18:47 -0400

    Use componenet to add emoji

---
+++ src/components/EmojiPicker/EmojiPicker.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+// Components
+import { Emoji, Picker } from "emoji-mart";
+
+export default function EmojiPicker(props) {
+  const { addEmoji } = props;
+
+  const [showPicker, setShowPicker] = useState(false);
+
+  const handleClick = () => setShowPicker(!showPicker);
+
+  return (
+    <span onClick={handleClick} style={{ position: "relative" }}>
+      <Emoji emoji="smile" size={16} />
+      <Picker
+        style={{
+          position: "absolute",
+          bottom: "20px",
+          right: "20px",
+          display: showPicker ? "block" : "none",
+        }}
+        onSelect={(emoji) => addEmoji(emoji)}
+      />
+    </span>
+  );
+}
