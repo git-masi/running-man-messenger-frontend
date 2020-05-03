5ec7c28087713091d3fab96229c3442a43134226
Eric Masi <eric@gitmasi.com>
2020-05-03 11:18:14 -0400

    Add option to set value directly

--- src/hooks/useInputState.js
+++ src/hooks/useInputState.js
@@ -4,5 +4,6 @@ export default (initialValue) => {
   const [state, setState] = useState(initialValue);
   const handleChange = (e) => setState(e.target.value);
   const reset = (resetValue = "") => setState(resetValue);
-  return [state, handleChange, reset];
+  const handleValue = (value) => setState(value);
+  return [state, handleChange, reset, handleValue];
 };
