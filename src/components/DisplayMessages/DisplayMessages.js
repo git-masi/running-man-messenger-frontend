c20505ddf3100f8fef81b9878270b618e2d776e9
Eric Masi <eric@gitmasi.com>
2020-05-03 09:46:49 -0400

    Dynamically add class if message is from sending user

--- src/components/DisplayMessages/DisplayMessages.js
+++ src/components/DisplayMessages/DisplayMessages.js
@@ -3,10 +3,17 @@ import React from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
 
 export default function DisplayMessages(props) {
-  const { messages } = props;
+  const { name, messages } = props;
+
+  const normalizeString = (str) => str.trim().toLowerCase();
 
   const displayMessages = messages.map((m) => (
-    <p key={m.createdAt}>
+    <p
+      key={m.createdAt}
+      className={
+        normalizeString(name) === normalizeString(m.name) ? "isUser" : null
+      }
+    >
       {m.name} &mdash; {m.text}
       <span style={{ fontSize: "12px", color: "#666", display: "block" }}>
         {m.createdAt}
