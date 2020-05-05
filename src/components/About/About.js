7179d4e7459da38f2ebdc329a16fee59a73ea565
Eric Masi <eric@gitmasi.com>
2020-05-05 11:05:10 -0400

    Add About component

---
+++ src/components/About/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      Icons made by{" "}
+      <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+        Freepik
+      </a>{" "}
+      from{" "}
+      <a href="https://www.flaticon.com/" title="Flaticon">
+        {" "}
+        www.flaticon.com
+      </a>
+    </div>
+  );
+}
