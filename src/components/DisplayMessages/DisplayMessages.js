9c2e89b2e8930ce62baecd62a9bf703e3f4cc64d
Eric Masi <eric@gitmasi.com>
2020-05-21 09:20:08 -0400

    Scroll to end on new message if user at already at end
    If the user scrolls up to look at another message we don't want to force them
    down to the bottom when a new message comes in.
    
    Rather than do some nasty scroll/wheel/touchMove event handling
    I have set up a hidden element and I am using intersection observer to
    determine if the user is at the bottom of the messages

--- src/components/DisplayMessages/DisplayMessages.js
+++ src/components/DisplayMessages/DisplayMessages.js
@@ -1,5 +1,5 @@
 // Modules
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 // CSS
@@ -8,9 +8,34 @@ import styles from './DisplayMessages.module.css';
 export default function DisplayMessages(props) {
   const { name, messages } = props;
 
+  const [userAtEndOfMessages, setUserAtEndOfMessages] = useState(true);
+
   const normalizeString = (str) => str.trim().toLowerCase();
 
   const messageContainer = useRef(null);
+  const endOfMessages = useRef(null);
+
+  useEffect(() => {
+    const callback = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setUserAtEndOfMessages(true);
+        } else {
+          setUserAtEndOfMessages(false);
+        }
+      });
+    };
+
+    let options = {
+      root: messageContainer.current,
+      rootMargin: '0px',
+      threshold: 0.9,
+    };
+
+    let observer = new IntersectionObserver(callback, options);
+
+    observer.observe(endOfMessages.current);
+  }, []);
 
   useEffect(() => {
     const el = messageContainer.current;
@@ -19,8 +44,9 @@ export default function DisplayMessages(props) {
     const clientHeight =
       el.clientHeight; /** This number is stactic, based on height in browser */
 
-    if (scrollHeight - clientHeight) el.scrollTo(0, scrollHeight);
-  }, [messages]);
+    if (scrollHeight - clientHeight && userAtEndOfMessages)
+      el.scrollTo(0, scrollHeight);
+  }, [messages, userAtEndOfMessages]);
 
   const displayMessages = messages.map((m) => (
     <p
@@ -39,6 +65,7 @@ export default function DisplayMessages(props) {
   return (
     <div ref={messageContainer} className={styles.messages}>
       {displayMessages}
+      <div ref={endOfMessages} className={styles.endOfMessages}></div>
     </div>
   );
 }
