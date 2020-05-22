89e33416634456581b8e5a9e1909fd8dc307d54b
Eric Masi <eric@gitmasi.com>
2020-05-22 08:32:06 -0400

    Use HashRouter for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // Packages
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Components
 import Join from './components/Join/Join';
@@ -12,7 +12,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename='/'>
       <Switch>
         <Route
           path='/about'
