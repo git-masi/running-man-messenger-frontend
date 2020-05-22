724cc94adb58c533689681a1c8a7d0ac7e705857
Eric Masi <eric@gitmasi.com>
2020-05-22 08:25:10 -0400

    Updates

---
+++ precache-manifest.ae9bed5c60065e32ffe89a25e8c7b1de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84677e36cec22c93a4308e047d445263",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3128e5fbb0c148cfbb0f",
+    "url": "/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "e1c684010133a0e8292e",
+    "url": "/static/css/main.73361734.chunk.css"
+  },
+  {
+    "revision": "3128e5fbb0c148cfbb0f",
+    "url": "/static/js/2.160319d7.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.160319d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1c684010133a0e8292e",
+    "url": "/static/js/main.2eb8a27f.chunk.js"
+  },
+  {
+    "revision": "66f6f5f80905193edd1a",
+    "url": "/static/js/runtime-main.e65e0d06.js"
+  }
+]);
