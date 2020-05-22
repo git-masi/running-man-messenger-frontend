9080160f935d83ad96123e03228fb0a276920cd0
Eric Masi <eric@gitmasi.com>
2020-05-22 08:32:39 -0400

    Updates

--- precache-manifest.777c39c87a56a8c3b80e2ddf835d32bf.js
+++ precache-manifest.777c39c87a56a8c3b80e2ddf835d32bf.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "84677e36cec22c93a4308e047d445263",
+    "revision": "bbb63ba2dd78d115ca75edf48721eb81",
     "url": "/index.html"
   },
   {
-    "revision": "3128e5fbb0c148cfbb0f",
+    "revision": "71cb3f8e69f083601797",
     "url": "/static/css/2.c2bc211b.chunk.css"
   },
   {
-    "revision": "e1c684010133a0e8292e",
+    "revision": "eccf9ad88b1dd7ca5b2a",
     "url": "/static/css/main.73361734.chunk.css"
   },
   {
-    "revision": "3128e5fbb0c148cfbb0f",
-    "url": "/static/js/2.160319d7.chunk.js"
+    "revision": "71cb3f8e69f083601797",
+    "url": "/static/js/2.d4bd2fb1.chunk.js"
   },
   {
     "revision": "b67bc97fd0dd67004287af4028afc1b1",
-    "url": "/static/js/2.160319d7.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.d4bd2fb1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e1c684010133a0e8292e",
-    "url": "/static/js/main.2eb8a27f.chunk.js"
+    "revision": "eccf9ad88b1dd7ca5b2a",
+    "url": "/static/js/main.bdc4d3ec.chunk.js"
   },
   {
     "revision": "66f6f5f80905193edd1a",
