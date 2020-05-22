41ddffb1b4b644478ab27dee62e72578515076c2
Eric Masi <eric@gitmasi.com>
2020-05-22 08:53:14 -0400

    Add homepage for deploy

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL || '/'}>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route
           path='/about'
